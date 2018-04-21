14e307bc27d471d4cb0793ad38fbbd7708853534
Filip Stolfa <phillipstolfa@gmail.com>
2018-04-21 10:31:39 +0200

    Require user to be logged in

--- src/SubjectAddForm.js
+++ src/SubjectAddForm.js
@@ -3,6 +3,8 @@ import { Modal, Button, Icon, Form, Message } from 'semantic-ui-react'
 
 import { db } from './firebase'
 
+import { UserContext } from './App'
+
 class SubjectAddForm extends Component {
   constructor(props){
     super(props);
@@ -68,37 +70,40 @@ class SubjectAddForm extends Component {
     } = this.state;
 
     return (
-      <Modal
-        trigger={<Button animated='fade' onClick={this.handleOpen}>
-                  <Button.Content visible>Add a Subject</Button.Content>
-                  <Button.Content hidden><Icon name='plus'/></Button.Content>
-                </Button>}
-        open={open}
-        onClose={this.handleClose}
-        dimmer={false}
-        closeOnDocumentClick
-      >
-        <Modal.Header>Add a Subject</Modal.Header>
-        <Modal.Content>
-          <p>Hi there! Let's add some subjects, shall we?</p>
-          <Form error={isError} onSubmit={this.handleAdd}>
-            <Form.Group widths='equal'>
-              <Form.Input value={name} onChange={(e) => this.setState({ name: e.target.value })} fluid label='Subject Name' placeholder='eg. Maths, Biology, ...'/>
-              <Form.Input value={initials} onChange={(e) => this.setState({ initials: e.target.value })} fluid label='Subject Initials' placeholder='eg. M, Ma, Bi, Fy, ...'/>
-              <Form.Input value={teacher} onChange={(e) => this.setState({ teacher: e.target.value })} fluid label='Teacher' placeholder='eg. Smith, Doe, ...'/>
-            </Form.Group>
-            <Message 
-              error
-              header='All fields are required'
-              content='Please fill in all fields of the form.'
-            />
-          </Form>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button basic onClick={this.handleClose}>Cancel</Button>
-          <Button positive onClick={this.handleAdd}>Add</Button>
-        </Modal.Actions>
-      </Modal>
+      <UserContext.Consumer>
+
+        <Modal
+          trigger={<Button animated='fade' onClick={this.handleOpen}>
+                    <Button.Content visible>Add a Subject</Button.Content>
+                    <Button.Content hidden><Icon name='plus'/></Button.Content>
+                  </Button>}
+          open={open}
+          onClose={this.handleClose}
+          dimmer={false}
+          closeOnDocumentClick
+        >
+          <Modal.Header>Add a Subject</Modal.Header>
+          <Modal.Content>
+            <p>Hi there! Let's add some subjects, shall we?</p>
+            <Form error={isError} onSubmit={this.handleAdd}>
+              <Form.Group widths='equal'>
+                <Form.Input value={name} onChange={(e) => this.setState({ name: e.target.value })} fluid label='Subject Name' placeholder='eg. Maths, Biology, ...'/>
+                <Form.Input value={initials} onChange={(e) => this.setState({ initials: e.target.value })} fluid label='Subject Initials' placeholder='eg. M, Ma, Bi, Fy, ...'/>
+                <Form.Input value={teacher} onChange={(e) => this.setState({ teacher: e.target.value })} fluid label='Teacher' placeholder='eg. Smith, Doe, ...'/>
+              </Form.Group>
+              <Message 
+                error
+                header='All fields are required'
+                content='Please fill in all fields of the form.'
+              />
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button basic onClick={this.handleClose}>Cancel</Button>
+            <Button positive onClick={this.handleAdd}>Add</Button>
+          </Modal.Actions>
+        </Modal>
+      </UserContext.Consumer>
     )
   }
 }
